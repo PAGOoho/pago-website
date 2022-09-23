a09d257fcaeee7343e871f60ea4c587e5cabdd75
PAGOoho <pagodamnit@gmail.com>
2022-09-23 13:09:54 +0200

    added 404 styles

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,15 +1,24 @@
 import { FaHome } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
+import { motion } from 'framer-motion';
+
 function NotFound() {
   return (
-    <div>
-      <h1>Oops!</h1>
-      <p>404 - Page Not Found!</p>
-      <Link to="/">
-        <FaHome />
-        Back To Home
-      </Link>
+    <div className="notfound">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.4 }}
+        className="notfoundwrap"
+      >
+        <h1>Oops!</h1>
+        <h2>404 - Page Not Found!</h2>
+        <Link className="btn" to="/">
+          <FaHome />
+          Back To Home
+        </Link>
+      </motion.div>
     </div>
   );
 }
